271a120a497e3906538c4251bec0d01b9da5c446
Xiaocong Ai <xiaocong.ai@cern.ch>
2021-06-24 13:25:11 +0200

    update for new geometry

--- script/plot_1D_features_defaultGeometry.py
+++ script/plot_1D_features_defaultGeometry.py
@@ -2,17 +2,12 @@ from ROOT import *
 #import ROOT
 import numpy as np
 
-nbins0x = 4; 
-nbins1x = 4;
-nbins2x = 16;
-nbins3x = 16;
-
-nbins0y = 16;
-nbins1y = 128;
-nbins2y = 16;
-nbins3y = 8;
-
-lvl0 = nbins0x * nbins0y;
+nbins1x = 3;
+nbins2x = 12;
+nbins3x = 12;
+nbins1y = 96;
+nbins2y = 12;
+nbins3y = 6;
 lvl1 = nbins1x * nbins1y;
 lvl2 = nbins2x * nbins2y;
 lvl3 = nbins3x * nbins3y;
@@ -37,48 +32,40 @@ lvl3 = nbins3x * nbins3y;
 #Function to get the cell index
 #
 def get_z(myindex):
-    if myindex == 960:
+    if myindex == 504:
         return 0
-    elif myindex == 961:
+    elif myindex == 505:
         return 1
-    elif myindex == 962:
+    elif myindex == 506:
         return 2
-    elif myindex == 963:
-        return 3 
-    elif (myindex >= lvl0 + lvl1 + lvl2):
-        return 3 
-    elif (myindex >= lvl0 + lvl1):
-        return 2 
-    elif (myindex >= lvl0):
-        return 1 
+    elif (myindex >= lvl1 + lvl2):
+        return 2
+    elif (myindex >= lvl1):
+        return 1
     else:
         return 0
     pass
 
 def get_y(myindex,zbin):
-    if (myindex >=960):
+    if (myindex >=504):
         return -1
     elif (zbin==0):
-        return myindex % nbins0y
+        return myindex % nbins1y
     elif (zbin==1):
-        return (myindex - lvl0) % nbins1y
-    elif (zbin==2):
-        return (myindex - lvl0 - lvl1) % nbins2y
+        return myindex % nbins2y
     else:
-        return (myindex - lvl0 - lvl1 - lvl2) % nbins3y
+        return myindex % nbins3y
     pass
 
 def get_x(myindex,ybin,zbin):
-    if (myindex >=960):
+    if (myindex >=504):
         return -1
     elif (zbin==0):
-        return (myindex - ybin)/nbins0y
+        return (myindex - ybin)/nbins1y
     elif (zbin==1):
-        return (myindex - lvl0 - ybin)/nbins1y
-    elif (zbin==2):
-        return (myindex - lvl0 - lvl1 - ybin)/nbins2y
+        return (myindex - lvl1 - ybin)/nbins2y
     else:
-        return (myindex - lvl0 - lvl1 - lvl2 - ybin)/nbins3y
+        return (myindex - lvl1 - lvl2 - ybin)/nbins3y
     pass
 
 
@@ -111,6 +98,7 @@ def setYRange(hists,yMinScale = 0.5, yMaxScale = 2.0):
 
 
 
+
 gStyle.SetOptFit(0011);
 gStyle.SetOptStat(0000);
 gStyle.SetPadLeftMargin(0.12);
@@ -120,25 +108,27 @@ gStyle.SetPadBottomMargin(0.15);
 
 histDict = {}
 
-inputDir="/nfs/dust/atlas/user/xiaocong/photonJet/generation/output/06-24-21-00/root/"
+inputDir="/nfs/dust/atlas/user/xiaocong/photonJet/generation/output/05-26-21-21/root/"
+#inputFiles = [ "axion1_100GeV_20k.root", "axion1_500GeV_20k.root", "axion1_1TeV_20k.root" ]
+#inputFiles = [ "electron_100GeV_20k.root", "electron_500GeV_20k.root", "electron_1TeV_20k.root" ]
 
-inputFiles = [ "axion1_60-500GeV_20k.root", "axion2_60-500GeV_20k.root", "scalar1_60-500GeV_20k.root" ]
+#inputFiles = [ "axion1_100GeV_20k.root", "axion2_100GeV_20k.root", "scalar1_100GeV_20k.root", "gamma_100GeV_20k.root", "pi0_100GeV_20k.root", "electron_100GeV_20k.root" ]
+inputFiles = [ "axion1_500GeV_20k.root", "axion2_500GeV_20k.root", "scalar1_500GeV_20k.root", "gamma_500GeV_20k.root", "pi0_500GeV_20k.root", "electron_500GeV_20k.root" ]
+#inputFiles = [ "axion1_1TeV_20k.root", "axion2_1TeV_20k.root", "scalar1_1TeV_20k.root", "gamma_1TeV_20k.root", "pi0_1TeV_20k.root", "electron_1TeV_20k.root" ]
 
 outputDir="/nfs/dust/atlas/user/xiaocong/photonJet/generation/plots/"
 
 #suffix="axion1_"
 #suffix="electron_"
 #suffix="100GeV_"
-suffix="60-500GeV_"
+suffix="500GeV_"
 #suffix="1TeV_"
 
 Fraction_in_thirdlayer = []
 Fraction_not_in = []
-PreSam_lateral = []
 Front_lateral = []
 Middle_lateral = []
 Back_lateral = []
-PreSam_lateral_width = []
 Front_lateral_width = []
 Middle_lateral_width = []
 Back_lateral_width = []
@@ -146,7 +136,7 @@ Shower_Depth = []
 Shower_Depth_width = []
 
 legs = []
-for i in range(12):
+for i in range(10):
     legs.append(TLegend(.45,.65,.7,.9))
     legs[i].SetBorderSize(0)
     legs[i].SetFillColor(0)
@@ -171,30 +161,26 @@ for iFile in range(len(inputFiles)):
 	#photons-10GeV-1k.root
 	mytree = tfile_open[iFile].Get("fancy_tree")
 
-	zsegmentation = TH1F("","",4,np.array([1464.148, 1500, 1590.684, 1928.113, 1970.292]))
+	zsegmentation = TH1F("","",3,np.array([-240.,-150.,197.,240.]))
 	zsegmentation.GetXaxis().SetTitle("Layer");
 	zsegmentation.GetYaxis().SetTitle("Energy [GeV]");
 	setHistStyle(zsegmentation,colors[iFile],markers[iFile])
-	#Layer0: 4x16 (phixeta)
-	sampling0_eta = TH2F("","",4,-0.1*2,0.1*2, 16,-0.025*8, 0.025*8)
-	sampling0_eta.GetXaxis().SetTitle("Phi");
-	sampling0_eta.GetYaxis().SetTitle("Eta");
-	#Layer1: 4x128 (phixeta)
-	sampling1_eta = TH2F("","",4,-0.098*2,0.098*2, 128,-0.003125*64, 0.003125*64)
+	#Layer1: 3x96 (phixeta)
+	sampling1_eta = TH2F("","",3,-240.,240.,96,-240.,240.)
 	sampling1_eta.GetXaxis().SetTitle("Phi");
 	sampling1_eta.GetYaxis().SetTitle("Eta");
-	#Layer2: 16x16 (phixeta)
-	sampling2_eta = TH2F("","",16,-0.0245*8, 0.0245*8, 16,-0.025*8, 0.025*8)
+	#Layer2: 12x12 (phixeta)
+	sampling2_eta = TH2F("","",12,-240.,240.,12,-240.,240.)
 	sampling2_eta.GetXaxis().SetTitle("Phi");
 	sampling2_eta.GetYaxis().SetTitle("Eta");
-	#Layer3: 16x8 (phixeta)
-	sampling3_eta = TH2F("","",16,-0.0245*8, 0.0245*8, 8,-0.05*4, 0.05*4)
+	#Layer3: 12x6 (phixeta)
+	sampling3_eta = TH2F("","",12,-240.,240.,6,-240.,240.)
 	sampling3_eta.GetXaxis().SetTitle("Phi");
 	sampling3_eta.GetYaxis().SetTitle("Eta");
 
         #Plots will be superimposed on them
         #Fraction_in_thirdlayer.append(TH1F("Fraction_in_thirdlayer_{0}".format(outputFile),"Fraction_in_thirdlayer_{0}".format(outputFile),100,0,0.01))
-        Fraction_in_thirdlayer.append(TH1F("Fraction_in_thirdlayer_{0}".format(outputFile),"",100,0,1))
+        Fraction_in_thirdlayer.append(TH1F("Fraction_in_thirdlayer_{0}".format(outputFile),"",100,0,0.01))
         Fraction_in_thirdlayer[iFile].GetXaxis().SetTitle("Fraction");
         Fraction_in_thirdlayer[iFile].GetYaxis().SetTitle("Entries");
         setHistStyle(Fraction_in_thirdlayer[iFile],colors[iFile],markers[iFile])
@@ -203,43 +189,34 @@ for iFile in range(len(inputFiles)):
         Fraction_not_in[iFile].GetYaxis().SetTitle("Entries");
         setHistStyle(Fraction_not_in[iFile],colors[iFile],markers[iFile])
             
-        PreSam_lateral.append(TH1F("PreSampling_lateral_{0}".format(outputFile),"",30,-0.2,0.2))
-        PreSam_lateral[iFile].GetXaxis().SetTitle("Energy weighted size");
-        PreSam_lateral[iFile].GetYaxis().SetTitle("Entries");
-        setHistStyle(PreSam_lateral[iFile],colors[iFile],markers[iFile])
-        PreSam_lateral_width.append(TH1F("PreSampling_lateral_width_{0}".format(outputFile),"",50,0,0.2))
-        PreSam_lateral_width[iFile].GetXaxis().SetTitle("Sigma of energy weighted size");
-        PreSam_lateral_width[iFile].GetYaxis().SetTitle("Entries");
-        setHistStyle(PreSam_lateral_width[iFile],colors[iFile],markers[iFile])
-
-        Front_lateral.append(TH1F("Front_lateral_{0}".format(outputFile),"",30,-0.2,0.2))
+        Front_lateral.append(TH1F("Front_lateral_{0}".format(outputFile),"",30,-240,240))
         Front_lateral[iFile].GetXaxis().SetTitle("Energy weighted size");
         Front_lateral[iFile].GetYaxis().SetTitle("Entries");
         setHistStyle(Front_lateral[iFile],colors[iFile],markers[iFile])
-        Front_lateral_width.append(TH1F("Front_lateral_width_{0}".format(outputFile),"",50,0,0.2))
+        Front_lateral_width.append(TH1F("Front_lateral_width_{0}".format(outputFile),"",100,0,100))
         Front_lateral_width[iFile].GetXaxis().SetTitle("Sigma of energy weighted size");
         Front_lateral_width[iFile].GetYaxis().SetTitle("Entries");
         setHistStyle(Front_lateral_width[iFile],colors[iFile],markers[iFile])
 
-        Middle_lateral.append(TH1F("Middle_lateral_{0}".format(outputFile),"",120,-0.2,0.2))
+        Middle_lateral.append(TH1F("Middle_lateral_{0}".format(outputFile),"",120,-240,240))
         Middle_lateral[iFile].GetXaxis().SetTitle("Energy weighted size");
         Middle_lateral[iFile].GetYaxis().SetTitle("Entries");
         setHistStyle(Middle_lateral[iFile],colors[iFile],markers[iFile])
-        Middle_lateral_width.append(TH1F("Middle_lateral_width_{0}".format(outputFile),"",200,0,0.2))
+        Middle_lateral_width.append(TH1F("Middle_lateral_width_{0}".format(outputFile),"",80,20,40))
         Middle_lateral_width[iFile].GetXaxis().SetTitle("Sigma of energy weighted size");
         Middle_lateral_width[iFile].GetYaxis().SetTitle("Entries");
         setHistStyle(Middle_lateral_width[iFile],colors[iFile],markers[iFile])
 
-        Back_lateral.append(TH1F("Back_lateral_{0}".format(outputFile),"",120,-0.2,0.2))
+        Back_lateral.append(TH1F("Back_lateral_{0}".format(outputFile),"",120,-240,240))
         Back_lateral[iFile].GetXaxis().SetTitle("Energy weighted size");
         Back_lateral[iFile].GetYaxis().SetTitle("Entries");
         setHistStyle(Back_lateral[iFile],colors[iFile],markers[iFile])
-        Back_lateral_width.append(TH1F("Back_lateral_width_{0}".format(outputFile),"",200,0,0.2))
+        Back_lateral_width.append(TH1F("Back_lateral_width_{0}".format(outputFile),"",150,0,150))
         Back_lateral_width[iFile].GetXaxis().SetTitle("Sigma of energy weighted size");
         Back_lateral_width[iFile].GetYaxis().SetTitle("Entries");
         setHistStyle(Back_lateral_width[iFile],colors[iFile],markers[iFile])
             
-        Shower_Depth.append(TH1F("Shower_Depth_{0}".format(outputFile),"",100,0,2.5))
+        Shower_Depth.append(TH1F("Shower_Depth_{0}".format(outputFile),"",100,0,1.5))
         Shower_Depth[iFile].GetXaxis().SetTitle("Energy weighted depth");
         Shower_Depth[iFile].GetYaxis().SetTitle("Entries");
         setHistStyle(Shower_Depth[iFile],colors[iFile],markers[iFile])
@@ -251,20 +228,18 @@ for iFile in range(len(inputFiles)):
         # Add legend entries
         legs[0].AddEntry(Fraction_in_thirdlayer[iFile], outputFile, "l") 
         legs[1].AddEntry(Fraction_not_in[iFile], outputFile, "l") 
-        legs[2].AddEntry(PreSam_lateral[iFile], outputFile, "l") 
-        legs[3].AddEntry(Front_lateral[iFile], outputFile, "l") 
-        legs[4].AddEntry(Middle_lateral[iFile], outputFile, "l") 
-        legs[5].AddEntry(Back_lateral[iFile], outputFile, "l") 
-        legs[6].AddEntry(PreSam_lateral_width[iFile], outputFile, "l") 
-        legs[7].AddEntry(Front_lateral_width[iFile], outputFile, "l") 
-        legs[8].AddEntry(Middle_lateral_width[iFile], outputFile, "l") 
-        legs[9].AddEntry(Back_lateral_width[iFile], outputFile, "l") 
-        legs[10].AddEntry(Shower_Depth[iFile], outputFile, "l") 
-        legs[11].AddEntry(Shower_Depth_width[iFile], outputFile, "l") 
+        legs[2].AddEntry(Front_lateral[iFile], outputFile, "l") 
+        legs[3].AddEntry(Middle_lateral[iFile], outputFile, "l") 
+        legs[4].AddEntry(Back_lateral[iFile], outputFile, "l") 
+        legs[5].AddEntry(Front_lateral_width[iFile], outputFile, "l") 
+        legs[6].AddEntry(Middle_lateral_width[iFile], outputFile, "l") 
+        legs[7].AddEntry(Back_lateral_width[iFile], outputFile, "l") 
+        legs[8].AddEntry(Shower_Depth[iFile], outputFile, "l") 
+        legs[9].AddEntry(Shower_Depth_width[iFile], outputFile, "l") 
 	
 	#Loop over all events
-        for i in range(min(10,mytree.GetEntries())):
-        #for i in range(mytree.GetEntries()):
+        #for i in range(min(10,mytree.GetEntries())):
+        for i in range(mytree.GetEntries()):
 	    mytree.GetEntry(i)
 	    if (i%100==0):
 	        print i,mytree.GetEntries()
@@ -272,7 +247,6 @@ for iFile in range(len(inputFiles)):
 	    y = "energy"
 	    exec("%s = %s" % (y,"mytree.cell_0"))
 	    total_energy = 0.
-	    pre_energy = 0.
 	    front_energy = 0.
 	    middle_energy = 0.
 	    back_energy = 0.
@@ -281,8 +255,6 @@ for iFile in range(len(inputFiles)):
 	    lateral_depth = 0.
 	    lateral_depth2 = 0.
 	    # Energy weighted X in first, second and third layers
-	    pre_layer_X = 0.
-	    pre_layer_X2 = 0.
 	    first_layer_X = 0.
 	    first_layer_X2 = 0.
 	    second_layer_X = 0.
@@ -290,7 +262,7 @@ for iFile in range(len(inputFiles)):
 	    third_layer_X = 0.
 	    third_layer_X2 = 0.
 	    #Loop over all cells in the event
-	    for j in range(964):
+	    for j in range(507):
 	        exec("%s = %s" % (y,"mytree.cell_"+str(j)))
 	        xbin = get_x(j,get_y(j,get_z(j)),get_z(j))
 	        ybin = get_y(j,get_z(j))
@@ -306,20 +278,13 @@ for iFile in range(len(inputFiles)):
 	            not_in+=energy
 	            pass
 	        if (zbin==0):
-	            sampling0_eta.Fill(sampling0_eta.GetXaxis().GetBinCenter(xbin+1),sampling0_eta.GetYaxis().GetBinCenter(ybin+1),energy)
-	            xvalue = sampling0_eta.GetXaxis().GetBinCenter(xbin+1)
-	            yvalue = sampling0_eta.GetYaxis().GetBinCenter(ybin+1)
-	            pre_layer_X += xvalue*energy
-	            pre_layer_X2 += xvalue*xvalue*energy
-	            pre_energy+=energy
-	        elif (zbin==1):
 	            sampling1_eta.Fill(sampling1_eta.GetXaxis().GetBinCenter(xbin+1),sampling1_eta.GetYaxis().GetBinCenter(ybin+1),energy)
 	            xvalue = sampling1_eta.GetXaxis().GetBinCenter(xbin+1)
 	            yvalue = sampling1_eta.GetYaxis().GetBinCenter(ybin+1)
 	            first_layer_X += xvalue*energy
 	            first_layer_X2 += xvalue*xvalue*energy
 	            front_energy+=energy
-	        elif (zbin==2):
+	        elif (zbin==1):
 	            sampling2_eta.Fill(sampling2_eta.GetXaxis().GetBinCenter(xbin+1),sampling2_eta.GetYaxis().GetBinCenter(ybin+1),energy)
 	            xvalue = sampling2_eta.GetXaxis().GetBinCenter(xbin+1)
 	            yvalue = sampling2_eta.GetYaxis().GetBinCenter(ybin+1)
@@ -338,11 +303,6 @@ for iFile in range(len(inputFiles)):
 	    Fraction_in_thirdlayer[iFile].Fill(back_energy/total_energy)
 	    Fraction_not_in[iFile].Fill(not_in/total_energy)
 	    Shower_Depth[iFile].Fill(lateral_depth/total_energy)
-
-	    if (pre_energy > 0):
-	        PreSam_lateral[iFile].Fill(pre_layer_X/pre_energy)
-	        PreSam_lateral_width[iFile].Fill((pre_layer_X2/pre_energy - (pre_layer_X/pre_energy)**2)**0.5)
-	        pass
 	    if (front_energy > 0):
 	        Front_lateral[iFile].Fill(first_layer_X/front_energy)
 	        Front_lateral_width[iFile].Fill((first_layer_X2/front_energy - (first_layer_X/front_energy)**2)**0.5)
@@ -364,7 +324,7 @@ for iFile in range(len(inputFiles)):
             histDict[outputFile] = []
             hdict = histDict[outputFile]
  
-        histDict[outputFile] = [ Fraction_in_thirdlayer[iFile], Fraction_not_in[iFile],  PreSam_lateral[iFile], Front_lateral[iFile], Middle_lateral[iFile],  Back_lateral[iFile], PreSam_lateral_width[iFile], Front_lateral_width[iFile], Middle_lateral_width[iFile], Back_lateral_width[iFile], Shower_Depth[iFile], Shower_Depth_width[iFile] ] 
+        histDict[outputFile] = [ Fraction_in_thirdlayer[iFile], Fraction_not_in[iFile], Front_lateral[iFile], Middle_lateral[iFile],  Back_lateral[iFile], Front_lateral_width[iFile], Middle_lateral_width[iFile], Back_lateral_width[iFile], Shower_Depth[iFile], Shower_Depth_width[iFile] ] 
         
         c1Name = "c1_{0}".format(outputFile)
 	
@@ -393,12 +353,9 @@ for iFile in range(len(inputFiles)):
 
 setYRange(Fraction_in_thirdlayer, 0.5, 2.0)
 setYRange(Fraction_not_in, 0.5, 2.0)
-
-setYRange(PreSam_lateral, 0.5, 2.0)
 setYRange(Front_lateral, 0.5, 2.0)
 setYRange(Middle_lateral, 0.5, 2.0)
 setYRange(Back_lateral, 0.5, 2.0)
-setYRange(PreSam_lateral_width, 0.5, 2.0)
 setYRange(Front_lateral_width, 0.5, 2.0)
 setYRange(Middle_lateral_width, 0.5, 2.0)
 setYRange(Back_lateral_width, 0.5, 2.0) 
@@ -419,44 +376,36 @@ for hist in Fraction_not_in:
 legs[1].Draw()
 
 #canvas3
-c3 = TCanvas("c3_all","c3",1600,400)
-c3.Divide(4,1)
+c3 = TCanvas("c3_all","c3",1200,400)
+c3.Divide(3,1)
 c3.cd(1)
-for hist in PreSam_lateral:
+for hist in Front_lateral:
    hist.Draw("same")
 legs[2].Draw()
 c3.cd(2)
-for hist in Front_lateral:
+for hist in Middle_lateral:
    hist.Draw("same")
 legs[3].Draw()
 c3.cd(3)
-for hist in Middle_lateral:
-   hist.Draw("same")
-legs[4].Draw()
-c3.cd(4)
 for hist in Back_lateral:
    hist.Draw("same")
-legs[5].Draw()
+legs[4].Draw()
 
 # canvas4
-c4 = TCanvas("c4_all","c4",1600,400)
-c4.Divide(4,1)
+c4 = TCanvas("c4_all","c4",1200,400)
+c4.Divide(3,1)
 c4.cd(1)
-for hist in PreSam_lateral_width:
-   hist.Draw("same")
-legs[6].Draw()
-c4.cd(2)
 for hist in Front_lateral_width:
    hist.Draw("same")
-legs[7].Draw()
-c4.cd(3)
+legs[5].Draw()
+c4.cd(2)
 for hist in Middle_lateral_width:
    hist.Draw("same")
-legs[8].Draw()
-c4.cd(4)
+legs[6].Draw()
+c4.cd(3)
 for hist in Back_lateral_width:
    hist.Draw("same")
-legs[9].Draw()
+legs[7].Draw()
 
 
 # canvas5
@@ -465,11 +414,11 @@ c5.Divide(2,1)
 c5.cd(1)
 for hist in Shower_Depth:
     hist.Draw("same")
-legs[10].Draw()
+legs[8].Draw()
 c5.cd(2)
 for hist in Shower_Depth_width:
     hist.Draw("same")
-legs[11].Draw()
+legs[9].Draw()
 
 
 c2.Print(outputDir + suffix + "Fraction.pdf")
